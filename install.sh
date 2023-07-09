2fa37eb34197d0fcbd93f8efeef11d9c121c9c13
numarkpro <numarktop1gg@gmail.com>
2023-07-09 19:57:49 +0300

    1

---
+++ install.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+set -e
+
+######################################################################################
+#                                                                                    #
+#                IntHost installer                                                   #
+#                                                                                    #
+######################################################################################
+
+export GITHUB_SOURCE="v0.1"
+export SCRIPT_RELEASE="v0.1"
+export GITHUB_BASE_URL="https://raw.githubusercontent.com/thebublik2013/installer"
+
+LOG_PATH="/var/log/inthost-installer.log"
+
+# check for curl
+if ! [ -x "$(command -v curl)" ]; then
+  echo "* curl is required in order for this script to work."
+  echo "* install using apt (Debian and derivatives) or yum/dnf (CentOS)"
+  exit 1
+fi
+
+# Always remove lib.sh, before downloading it
+rm -rf /tmp/lib.sh
+curl -sSL -o /tmp/lib.sh "$GITHUB_BASE_URL"/"$GITHUB_SOURCE"/lib/lib.sh
+# shellcheck source=lib/lib.sh
+source /tmp/lib.sh
+
+execute() {
+  echo -e "\n\n* IntHost-installer $(date) \n\n" >>$LOG_PATH
+
+  [[ "$1" == *"canary"* ]] && export GITHUB_SOURCE="master" && export SCRIPT_RELEASE="canary"
+  update_lib_source
+  run_ui "${1//_canary/}" |& tee -a $LOG_PATH
+
+  if [[ -n $2 ]]; then
+    echo -e -n "* Installation of $1 completed. Do you want to proceed to $2 installation? (y/N): "
+    read -r CONFIRM
+    if [[ "$CONFIRM" =~ [Yy] ]]; then
+      execute "$2"
+    else
+      error "Installation of $2 aborted."
+      exit 1
+    fi
+  fi
+}
+
+welcome ""
+
+done=false
+while [ "$done" == false ]; do
+  options=(
+    "Install Wings"
+  )
+
+  actions=(
+    "wings"
+  )
+
+  output "What would you like to do?"
+
+  for i in "${!options[@]}"; do
+    output "[$i] ${options[$i]}"
+  done
+
+  echo -n "* Input 0-$((${#actions[@]} - 1)): "
+  read -r action
+
+  [ -z "$action" ] && error "Input is required" && continue
+
+  valid_input=("$(for ((i = 0; i <= ${#actions[@]} - 1; i += 1)); do echo "${i}"; done)")
+  [[ ! " ${valid_input[*]} " =~ ${action} ]] && error "Invalid option"
+  [[ " ${valid_input[*]} " =~ ${action} ]] && done=true && IFS=";" read -r i1 i2 <<<"${actions[$action]}" && execute "$i1" "$i2"
+done
+
+# Remove lib.sh, so next time the script is run the, newest version is downloaded.
+rm -rf /tmp/lib.sh
---
+++ installers/phpmyadmin.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+######################################################################################
+#                                                                                    #
+#                IntHost installer                                                   #
+#                                                                                    #
+######################################################################################
+
+# Check if script is loaded, load if not or fail otherwise.
+fn_exists() { declare -F "$1" >/dev/null; }
+if ! fn_exists lib_loaded; then
+  # shellcheck source=lib/lib.sh
+  source /tmp/lib.sh || source <(curl -sSL "$GITHUB_BASE_URL/$GITHUB_SOURCE"/lib/lib.sh)
+  ! fn_exists lib_loaded && echo "* ERROR: Could not load lib script" && exit 1
+fi
+
+# When #280 is merged
