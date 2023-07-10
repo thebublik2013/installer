e1fad1219a287c03e984ce50923d1fe6bd8d3590
thebublik2013 <thebublik2013@gmail.com>
2023-07-10 18:20:24 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,7 +30,7 @@ set -e
 
 export GITHUB_SOURCE="v0.1"
 export SCRIPT_RELEASE="v0.1"
-export GITHUB_BASE_URL="hhttps://github.com/thebublik2013/installer"
+export GITHUB_BASE_URL="https://github.com/thebublik2013/installer"
 
 LOG_PATH="/var/log/pterodactyl-installer.log"
 
